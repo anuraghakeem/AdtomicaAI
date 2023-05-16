cfc57c9390af318e8daa5284f073f69dd3380a25
Anurag Hakeem <anuraghakeem@anurags-mbp.lan>
2023-05-16 11:43:04 +0530

    tone of voice added

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,10 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [companyName, setCompanyName] = useState('');
+  const [companyDescription, setCompanyDescription] = useState('');
+  const [previousCopy, setPreviousCopy] = useState('');
+  const [responseCopy, setResponseCopy] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // fetch response to the api combining the chat log arrayof messages and seinding it as a message to localhost:3000 as a post
+    const response = await fetch("http://localhost:3080/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        // message: chatLog.map((message) => message.message).join("")
+        message: `I want you to act as an expert branding manager and marketing copywriter. Write the tone of voice for ${companyName}. ${companyName} does ${companyDescription}. Generate the brand tone of voice for ${companyName}. Keep the format as three points. Each point should be short and simple to understand. For your reference, here is one of their previous social media creative copy: ${previousCopy}.`
+      })
+    });
+    const data = await response.json();
+    if (data){
+      setResponseCopy(data.data.choices[0].text)
+    }
+    console.log(data)
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Generate Tone Of voice of your brand</h1>
+      {/* <button onClick={() => handleClick()}>Click Me</button> */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="company-name">Name of Company:</label>
+        <input
+          type="text"
+          id="company-name"
+          value={companyName}
+          onChange={(event) => setCompanyName(event.target.value)}
+        />
+
+        <label htmlFor="company-description">Description of Company:</label>
+        <textarea
+          id="company-description"
+          value={companyDescription}
+          onChange={(event) => setCompanyDescription(event.target.value)}
+        />
+
+        <label htmlFor="previous-copy">Previous Copy:</label>
+        <textarea
+          id="previous-copy"
+          value={previousCopy}
+          onChange={(event) => setPreviousCopy(event.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {!!responseCopy && responseCopy}
+      </div>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -9,21 +11,28 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-const port = 3080
+const app = express();
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true}))
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 3080;
 
 app.post('/', async (req, res) => {
+    const { message } = req.body;
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
+        // prompt: "Say this is a test",
+        prompt: `${message}`,
+        max_tokens: 100,
         temperature: 0,
     });
     console.log(response.data.choices[0].text)
     res.json({
         data: response.data
     })
-    }
+}
 );
 
 app.listen(port, () => {
